b6b6a1baddab5b24548d31dcf3d89fb45323bd74
RK <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2024-07-24 12:31:11 +0530

    updated

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-
 function Todo() {
 	const [input, setInput] = useState("");
 	const [Task, setTask] = useState([])
@@ -23,22 +22,30 @@ function Todo() {
 	};
 	return (
 		<div>
+
+
+			<p className='heading fs-2 wittgenstein' >To-Do List</p>
 			<form>
-				<input type="text" onChange={handleInput} />
-				<button onClick={addTask}>Add</button>
+				<div className='d-flex justify-content-center'>
+					<input className='textbox' type="text" onChange={handleInput} />
+					<button className="button btn btn-outline-secondary assistant" onClick={addTask}>Add Todo</button>
+				</div>
 			</form>
 			<div>
-				<ol>
-					{
-						Task.map((item, index) => {
-							return <li key={index}>{item} <button onClick={() => deleteTask(index)}>Delete</button></li>
+				{
+					Task.map((item, index) => {
+						return <ul class="container">
+									<div class="row" key={index}>
+										<div class="col-10 wittgenstein"><div>{item}</div></div>
+										<div class="col-2"><button className="btn btn-outline-secondary assistant" onClick={() => deleteTask(index)}>Delete</button></div>
+									</div>
+								</ul>
 
-							/* If write onClick={deleteTask(index)}, it would immediately invoke deleteTask with index as 
-							its argument when the component renders. This is not what you want because you want deleteTask 
-							to be called only when the button is clicked.*/
-						})
-					}
-				</ol>
+						/* If write onClick={deleteTask(index)}, it would immediately invoke deleteTask with index as 
+						its argument when the component renders. This is not what you want because you want deleteTask 
+						to be called only when the button is clicked.*/
+					})
+				}
 			</div>
 		</div>
 	)
